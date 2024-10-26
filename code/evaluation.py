a45bfc0f675ab112e2ea4b9edf814ece80a16255
steve amancha <steve.amancha@gmail.com>
2024-10-26 21:36:00 +0000

    passing data thru correctly to eval script. Need to grab metrics still

--- code/evaluation.py
+++ code/evaluation.py
@@ -6,29 +6,41 @@ import tarfile
 import joblib
 import numpy as np
 import pandas as pd
-import xgboost
+import xgboost as xgb
+
 
 from sklearn.metrics import mean_squared_error
 
-def evaluate_model(name, model, X_test):
+def evaluate_model(name, model, X_test, y_test):
     print(f"Evaluating model: {name}...")
-    y_pred = model.predict(X_test)
-    y_pred_prob = model.predict_proba(X_test)[:, 1] if hasattr(model, 'predict_proba') else model.decision_function(X_test)
+    predictions = model.predict(X_test)
 
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+    # Get probability predictions if the model supports it
+    if hasattr(model, 'predict_proba'):
+        y_pred_prob = model.predict_proba(X_test)[:, 1]
+    elif hasattr(model, 'decision_function'):
+        y_pred_prob = model.decision_function(X_test)
+    else:
+        # For XGBoost Booster model, get probability predictions using DMatrix
+        y_pred_prob = y_pred
+
+    accuracy = accuracy_score(y_test, predictions)
+    precision = precision_score(y_test, predictions)
+    recall = recall_score(y_test, predictions)
+    f1 = f1_score(y_test, predictions)
     roc_auc = roc_auc_score(y_test, y_pred_prob)
 
     print(f"Model {name} evaluation complete.")
     return {
-        "Model": name,
-        "Accuracy": accuracy,
-        "Precision": precision,
-        "Recall": recall,
-        "F1-Score": f1,
-        "AUC": roc_auc
+        "regression_metrics": {
+            "Model": name,
+            "Accuracy": accuracy,
+            "Precision": precision,
+            "Recall": recall,
+            "F1-Score": f1,
+            "AUC": roc_auc
+            # "mse": {"value": mse, "standard_deviation": std},
+        }
     }
 
 if __name__ == "__main__":
@@ -40,10 +52,16 @@ if __name__ == "__main__":
 
     test_path = "/opt/ml/processing/test/test.csv"
     df = pd.read_csv(test_path, header=None)
-    name= 'XGBoostMODEL'
-    print("df shape", df.shape)
-    print("df 0 ", df[0])
-    evaluation_metrics = evaluate_model(name, model, df)
+    print("test df shape", df.shape)
+
+    name= 'XGBoostModel'
+
+    y_test = df.iloc[:, 0].to_numpy()
+    df.drop(df.columns[0], axis=1, inplace=True)
+
+    X_test = xgb.DMatrix(df.values)
+    
+    evaluation_metrics = evaluate_model(name, model, X_test, y_test)
 
     output_dir = "/opt/ml/processing/evaluation"
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
@@ -51,23 +69,6 @@ if __name__ == "__main__":
     evaluation_path = f"{output_dir}/evaluation.json"
     with open(evaluation_path, "w") as f:
         f.write(json.dumps(evaluation_metrics))
-
-    # Code provided from lab example, add above proper code that outputs a file as model.tar.gz NO pickle
-    # model_path = f"/opt/ml/processing/model/model.tar.gz"
-    # with tarfile.open(model_path) as tar:
-    #     tar.extractall(path=".")
-
-    # model = pickle.load(open("xgboost-model", "rb"))
-
-    # test_path = "/opt/ml/processing/test/test.csv"
-    # df = pd.read_csv(test_path, header=None)
-
-    # y_test = df.iloc[:, 0].to_numpy()
-    # df.drop(df.columns[0], axis=1, inplace=True)
-
-    # X_test = xgboost.DMatrix(df.values)
-
-    # predictions = model.predict(X_test)
 
     # mse = mean_squared_error(y_test, predictions)
     # std = np.std(y_test - predictions)
--- code/preprocessing.py
+++ code/preprocessing.py
@@ -16,9 +16,10 @@ from sklearn.model_selection import train_test_split
 # Since we get a headerless CSV file, we specify the column names here.
 feature_columns_names = [
     "id",
+    "age",
     "gender",
-    "weight",
     "height",
+    "weight",
     "ap_hi",
     "ap_low",
     "cholesterol",
@@ -27,17 +28,18 @@ feature_columns_names = [
     "alco",
     "active"
 ]
-label_column = "rings"
+label_column = "cardio"
 
 categorical_features_names = [
-    "alco","active","smoke"
+    "gender"
 ]
 
 feature_columns_dtype = {
     "id": np.float64,
+    "age": np.float64,
     "gender": np.float64,
-    "weight": np.float64,
     "height": np.float64,
+    "weight": np.float64,
     "ap_hi": np.float64,
     "ap_low": np.float64,
     "cholesterol": np.float64,
@@ -47,8 +49,12 @@ feature_columns_dtype = {
     "active": np.float64
 }
 
-# label_column_dtype = {"rings": np.float64}
-# this might be the one to 
+label_column_dtype = {"cardio": np.float64}
+
+def merge_two_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
 
 
 if __name__ == "__main__":
@@ -58,40 +64,71 @@ if __name__ == "__main__":
     # local_path = base_dir + "/input/cardio-train.csv"
     
     # Accessing the whole raw dataset for preprocessing
-    local_path = base_dir + "/input/cardio-raw-kaggle.csv"
+    local_path = base_dir + "/input/cardio-raw-no-header.csv"
     
     # Download the file from S3 to the local directory and convert to dataframe
     # sagemaker.s3.S3Downloader.download(s3_uri, local_path)
-    df = pd.read_csv(local_path, sep=";")
-    print("dataframe size / shape", df.size, df.shape)
-    
-    print("Dataset loaded successfully. Now filling missing values with the median...")
-    df = df.fillna(df.median())  # Fill missing values
-    
-    print("Converting 'gender' column to numeric (1=male, 2=female) as needed...")
-    df['gender'] = df['gender'].map({1: 0, 2: 1})  # Convert gender to numeric
+    df = pd.read_csv(local_path,
+                     header=None,
+                     names=feature_columns_names + [label_column],
+                     dtype=merge_two_dicts(feature_columns_dtype, label_column_dtype))
 
-    X = df.drop(columns=['cardio'])  # Features
-    y = df['cardio']  # Target variable
+    numeric_features = list(feature_columns_names)
+    numeric_features.remove("gender")
+    numeric_transformer = Pipeline(
+        steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
+    )
+
+    categorical_features = ["gender"]
+    categorical_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("onehot", OneHotEncoder(handle_unknown="ignore")),
+        ]
+    )
+
+    preprocess = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, numeric_features),
+            ("cat", categorical_transformer, categorical_features),
+        ]
+    )
+
+    y = df.pop("cardio")
+    X_pre = preprocess.fit_transform(df)
+    y_pre = y.to_numpy().reshape(len(y), 1)
+
+    X = np.concatenate((y_pre, X_pre), axis=1)
+
+    np.random.shuffle(X)
+    train, validation, test = np.split(X, [int(0.7 * len(X)), int(0.85 * len(X))])
+    ######
     
-    # Split into training (60%) and temporary (40%)
-    print("Performing initial train-temp split with 60% training and 40% temporary...")
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
+    # print("dataframe size / shape", df.size, df.shape)
     
-    # Split the temporary set into validation (50%) and test (50%) sets
-    print("Splitting temporary set into validation and test sets (each 20%)...")
-    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+    # print("Dataset loaded successfully. Now filling missing values with the median...")
+    # df = df.fillna(df.median())  # Fill missing values
+    
+    # print("Converting 'gender' column to numeric (1=male, 2=female) as needed...")
+    # df['gender'] = df['gender'].map({1: 0, 2: 1})  # Convert gender to numeric
+
+    # X = df.drop(columns=['cardio'])  # Features
+    # y = df['cardio']  # Target variable
+    
+    # # Split into training (60%) and temporary (40%)
+    # print("Performing initial train-temp split with 60% training and 40% temporary...")
+    # X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
+    
+    # # Split the temporary set into validation (50%) and test (50%) sets
+    # print("Splitting temporary set into validation and test sets (each 20%)...")
+    # X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
     
     print("Train, validation, and test splits completed.")
-    print("Training set size:", X_train.shape)
-    print("Validation set size:", X_val.shape)
-    print("Test set size:", X_test.shape)
+    print("Training set size:", train.shape)
+    print("Validation set size:", validation.shape)
+    print("Test set size:", test.shape)
     
     # Writing locally to project
-    pd.DataFrame(X_train).to_csv(f"{base_dir}/train/train.csv", header=False, index=False)
-    pd.DataFrame(X_test).to_csv(f"{base_dir}/test/test.csv", header=False, index=False)
-    pd.DataFrame(X_val).to_csv(f"{base_dir}/validation/validation.csv", header=False, index=False)
-
-    pd.DataFrame(y_train).to_csv(f"{base_dir}/train-labels/train.csv", header=False, index=False)
-    pd.DataFrame(y_test).to_csv(f"{base_dir}/test-labels/test.csv", header=False, index=False)
-    pd.DataFrame(y_val).to_csv(f"{base_dir}/validation-labels/validation.csv", header=False, index=False)
+    pd.DataFrame(train).to_csv(f"{base_dir}/train/train.csv", header=False, index=False)
+    pd.DataFrame(test).to_csv(f"{base_dir}/test/test.csv", header=False, index=False)
+    pd.DataFrame(validation).to_csv(f"{base_dir}/validation/validation.csv", header=False, index=False)
